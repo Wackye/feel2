817c584d1968636d301438e77435b9ecbf8e592c
Wackye <j4170149@gmail.com>
2022-03-24 00:52:22 +0800

    add Reguler expression to filter code scan

--- unit_test/SerialTest.py
+++ unit_test/SerialTest.py
@@ -1,14 +1,17 @@
 import serial   
 from time import sleep
-
-# ser = serial.Serial("/dev/ttyS0", 9600)
+import re
+ser = serial.Serial("/dev/ttyS0", 9600)
 # ser = serial.Serial("/dev/tty1", 9600)
 while True:
     print('loop')
     recieved_data = ser.read()
     sleep(0.03)
     data_left = ser.inWaiting()
-    recieved_data += ser.read(data_left)
+    recieved_data += ser.read(data_left) 
 #    if(data_left > 0):#        recieved_data = str(int(recieved_data))
-    print(recieved_data)
+
+    pattern = r'[^0-9]+'
+    result = re.sub(pattern,'',str(recieved_data))
+    print(result)
     
